7f7ec244e5ae62177ab05d577224b86a87091aa0
alquerci <alquerci@email.com>
2013-02-20 22:37:49 +0100

    Update with new routing feature

--- src/acme/demo_bundle/__init__.py
+++ src/acme/demo_bundle/__init__.py
@@ -6,10 +6,28 @@
 #
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
-"""
-"""
 
 from pymfony.component.kernel.bundle import Bundle
+from pymfony.component.console.input import InputDefinition
+from pymfony.component.console_kernel.routing import Route
+from pymfony.component.console.input import InputArgument
+from pymfony.component.console.input import InputOption
+from pymfony.component.console_kernel.routing import RouteCollection
+
+"""
+"""
 
 class AcmeDemoBundle(Bundle):
-    pass;
+
+    def boot(self):
+
+        route = Route('hello', InputDefinition([
+            InputArgument("name", InputArgument.OPTIONAL, "Set your name", "Fabien"),
+            InputOption("--time", "-t", InputOption.VALUE_NONE, "Show the current timestamp time is it")
+        ]));
+        route.setDefault('_controller', "AcmeDemoBundle:Demo:hello");
+        route.setDescription("Say hello");
+
+        routeCollection = self._container.get('console.route_collection');
+        assert isinstance(routeCollection, RouteCollection);
+        routeCollection.add('hello', route);
--- src/acme/demo_bundle/dependency.py
+++ src/acme/demo_bundle/dependency.py
@@ -5,13 +5,10 @@
 #
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
-from pymfony.component.console.input import InputDefinition
-from pymfony.component.console.input import InputArgument
-from pymfony.component.console.input import InputOption
 """
 """
 
-from os import path
+from os.path import dirname;
 
 from pymfony.component.kernel.dependency import Extension
 from pymfony.component.dependency.loader import JsonFileLoader
@@ -19,28 +16,9 @@ from pymfony.component.config import FileLocator
 
 class AcmeDemoExtension(Extension):
     def load(self, configs, container):
-        loader = JsonFileLoader(container, FileLocator(path.dirname(__file__)+'/Resources/config'))
+        loader = JsonFileLoader(container, FileLocator(dirname(__file__)+'/Resources/config'))
         loader.load("services.json");
 
-        # command register TODO do a better way
-
-        container.setParameter('console.default_command', 'hello');
-
-        if container.hasParameter('console.commands'):
-            commands = container.getParameter('console.commands');
-        else:
-            commands = dict();
-
-        commands['hello'] = {
-            "_description": "Say hello",
-            "_controller": "AcmeDemoBundle:Demo:hello",
-            "_definition": InputDefinition([
-                InputArgument("name", InputArgument.OPTIONAL, "set your name", "fabien"),
-                InputOption("--time", "-t", InputOption.VALUE_NONE, "show the current timestamp time is it")
-            ])
-        };
-
-        container.setParameter('console.commands', commands);
 
     def getAlias(self):
         return "acme_demo";
