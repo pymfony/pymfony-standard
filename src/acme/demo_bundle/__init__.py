8e11438ee02071d9d5c1ce7d5a5b6f3976689816
alquerci <alquerci@email.com>
2013-02-26 02:35:40 +0100

    Load console routes with configuration file

--- src/acme/demo_bundle/__init__.py
+++ src/acme/demo_bundle/__init__.py
@@ -8,23 +8,9 @@
 # file that was distributed with this source code.
 
 from pymfony.component.kernel.bundle import Bundle
-from pymfony.component.console.input import InputDefinition
-from pymfony.component.console_kernel.routing import Route
-from pymfony.component.console.input import InputArgument
-from pymfony.component.console.input import InputOption
 
 """
 """
 
 class AcmeDemoBundle(Bundle):
-
-    def boot(self):
-
-        routeCollection = self._container.get('console.router').getRouteCollection();
-
-        routeCollection.add('acme_demo_hello', Route('hello', "Say hello", {
-            '_controller': "AcmeDemoBundle:Demo:hello"
-        },[
-            InputArgument("name", InputArgument.OPTIONAL, "Set your name", "Fabien"),
-            InputOption("--time", "-t", InputOption.VALUE_NONE, "Show the current timestamp time is it"),
-        ]));
+    pass;
